850ff9d459be87244897cce9fb2cba6b3857e1bd
EugeneHsiung <Eugene.hsiung@stonybrook.edu>
2023-11-22 22:10:54 +0000

    dev2 p1

--- model_dev2/scripts/p1_extract.py
+++ model_dev2/scripts/p1_extract.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 # Loading the dataset
-# Original link: https://catalog.data.gov/dataset/popular-baby-names/resource/02e8f55e-2157-4cb2-961a-2aabb75cbc8b
-datalink = 'https://data.cityofnewyork.us/api/views/25th-nujf/rows.csv?accessType=DOWNLOAD'
+# Original link: https://catalog.data.gov/dataset/2012-sat-results/resource/d6de35c7-7c4a-4b61-9fa1-232cba5d80af
+datalink = 'https://data.cdc.gov/api/views/e8kx-wbww/rows.csv?accessType=DOWNLOAD'
 df = pd.read_csv(datalink)
 print(df)
 
 df.size
 df.sample(5)
 
-# Saving as CSV file to model_dev1/data/raw
-df.to_csv('/home/eugenehsiung/datasci_9_data_prep/model_dev1/scripts/data/popular_data_names.csv', index=False)
+# Saving as CSV file to model_dev2/data/raw
+df.to_csv('/home/eugenehsiung/datasci_9_data_prep/model_dev2/data/raw/female_teen_birth_rates.csv', index=False)
 
-# Saving as Pickle file to model_dev1/data/raw
-df.to_pickle('/home/eugenehsiung/datasci_9_data_prep/model_dev1/scripts/data/popular_data_names.pkl')
+# Saving as Pickle file to model_dev2/data/raw
+df.to_pickle('/home/eugenehsiung/datasci_9_data_prep/model_dev2/data/raw/female_teen_birth_rates.pkl')
